44cab872f6287a4277aa09c1c6af4c1b4b7ea6dd
Sukanta Bhunia <113849325+sukantabhun@users.noreply.github.com>
2024-10-14 14:31:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'https://imagine-ai-93vv.vercel.app', // Allow only this origin
+  origin: 'https://imagine-ai-93vv.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],// Allow only this origin
 }));
 app.use(express.json({ limit: "50mb" }));
 
