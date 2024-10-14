a6c1c8f64c105d008f603887c8c96f6c060a00d4
Sukanta Bhunia <113849325+sukantabhun@users.noreply.github.com>
2024-10-14 16:48:42 +0530

    Update postRoutes.js

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -16,7 +16,6 @@ cloudinary.config({
 
 router.route('/').get(async (req,res) => {
     try {
-        res.send('Hello')
         const posts = await Post.find({});
         res.status(200).json({success: true, data:posts})
     } catch(error){
