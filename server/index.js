c243a2a4cd3155a1553c5b2c05fefaeb021e1571
Sukanta Bhunia <113849325+sukantabhun@users.noreply.github.com>
2024-10-14 15:35:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,22 +10,20 @@ dotenv.config();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; script-src 'self' https://vercel.live"
+  );
+  next();
+});
+
+
 // Middleware to log incoming requests
 app.use(cors());
 
 // CORS configuration
 
-// Handle preflight requests manually
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', corsOptions.origin);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(204);
-    }
-    next();
-});
 
 // Middleware to parse JSON requests
 app.use(express.json({ limit: "50mb" }));
