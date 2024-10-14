08651d5d1123a5c90beb3e46c9a23db40c64dbfa
Sukanta Bhunia <113849325+sukantabhun@users.noreply.github.com>
2024-10-14 14:23:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://imagine-ai-93vv.vercel.app/'],
   method: ["POST", "GET"],
   credentials: false
 }));
