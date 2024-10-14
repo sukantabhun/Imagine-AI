44ccc8ccbbe01d2420cd8a9dbf7d369a791ef968
Sukanta Bhunia <113849325+sukantabhun@users.noreply.github.com>
2024-10-14 14:17:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["*"],
+  method: ["POST", "GET"],
+  credentials: false
+}));
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
